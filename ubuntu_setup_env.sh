fb8bae2611e0fb2f5d4e3609417126f2aa614881
Vasily Romanikhin <vasily.romanikhin@gmail.com>
2018-03-13 15:22:18 +0300

    WSL: ubuntu - setup clion dev environment: cmake, compilers, debugger and ssh-server

---
+++ ubuntu_setup_env.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+SSHD_PORT=2222
+SSHD_FILE=/etc/ssh/sshd_config
+SUDOERS_FILE=/etc/sudoers
+  
+# 0. update package lists
+sudo apt-get update
+
+# 0.1. reinstall sshd (workaround for initial version of WSL)
+sudo apt remove -y --purge openssh-server
+sudo apt install -y openssh-server
+
+# 1.2. configure sshd
+sudo cp $SSHD_FILE ${SSHD_FILE}.`date '+%Y-%m-%d_%H-%M-%S'`.back
+sudo sed -i '/^Port/ d' $SSHD_FILE
+sudo sed -i '/^UsePrivilegeSeparation/ d' $SSHD_FILE
+sudo sed -i '/^PasswordAuthentication/ d' $SSHD_FILE
+echo "# configured by CLion"      | sudo tee -a $SSHD_FILE
+echo "Port ${SSHD_PORT}"          | sudo tee -a $SSHD_FILE
+echo "UsePrivilegeSeparation no"  | sudo tee -a $SSHD_FILE
+echo "PasswordAuthentication yes" | sudo tee -a $SSHD_FILE
+# 1.3. apply new settings
+sudo service ssh --full-restart
+  
+# 2. autostart: run sshd 
+sed -i '/^sudo service ssh --full-restart/ d' ~/.bashrc
+echo "%sudo ALL=(ALL) NOPASSWD: /usr/sbin/service ssh --full-restart" | sudo tee -a $SUDOERS_FILE
+cat << 'EOF' >> ~/.bashrc
+sshd_status=$(service ssh status)
+if [[ $sshd_status = *"is not running"* ]]; then
+  sudo service ssh --full-restart
+fi
+EOF
+  
+# 3. install basic dependencies
+sudo apt install -y cmake gcc clang gdb valgrind build-essential
+ 
+# summary: SSHD config info
+echo 
+echo "SSH server parameters ($SSHD_FILE):"
+echo "Port ${SSHD_PORT}"
+echo "UsePrivilegeSeparation no"
+echo "PasswordAuthentication yes"
